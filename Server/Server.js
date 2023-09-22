448776c474d0000ef4b2b7a616931de48da1074e
giovyc93 <118946227+giovyc93@users.noreply.github.com>
2023-09-22 12:49:58 +0200

    nuovo

--- Server/Server.js
+++ Server/Server.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-  host: "localhost",
+  host: "127.0.0.1",
   user: "root",
   password: "password",
   database: "database",
